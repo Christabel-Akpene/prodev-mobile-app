be3e6d28458fe1a4e72f84f8a95f535d01fd0030
Christabel-Akpene <dc.akpene@gmail.com>
2026-01-15 19:24:15 +0000

    chore: add pages

---
+++ prodev-mobile-app-0x05/app/(home)/_layout.tsx
@@ -0,0 +1,32 @@
+import { AntDesign, EvilIcons, Feather, FontAwesome, Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+
+const HomeRootLayout = () => {
+    return (
+        <Tabs screenOptions={{tabBarActiveTintColor: "#34967c", headerShown: false}}>
+        <Tabs.Screen name="index" options={{
+            title: "Home",
+            tabBarIcon: ({color}) => <AntDesign name="home" size={24} color={color} />
+        }}/>
+        <Tabs.Screen name="search" options={{
+            title: "Home",
+            tabBarIcon: ({color}) => <Feather name="search" size={24} color={color} />
+        }}/>
+        <Tabs.Screen name="saved" options={{
+            title: "Home",
+            tabBarIcon: ({color}) => <EvilIcons name="heart" size={24} color={color} />
+        }}/>
+        <Tabs.Screen name="inbox" options={{
+            title: "Home",
+            tabBarIcon: ({color}) => <Ionicons name="chatbubbles-outline" size={24} color={color} />
+        }}/>
+        <Tabs.Screen name="profile" options={{
+            title: "Home",
+            tabBarIcon: ({color}) => <FontAwesome name="user-o" size={24} color={color} />
+        }}/>
+
+        </Tabs>
+    )
+}
+
+export default HomeRootLayout;
