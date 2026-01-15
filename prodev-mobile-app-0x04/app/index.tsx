2342e43e57488b0adaf98335de35a530ebc80f66
Christabel-Akpene <dc.akpene@gmail.com>
2026-01-15 15:09:43 +0000

    feat: add pages with navigation

--- prodev-mobile-app-0x04/app/index.tsx
+++ prodev-mobile-app-0x04/app/index.tsx
@@ -1,15 +1,68 @@
-import { Text, View } from "react-native";
+import {
+  Image,
+  ImageBackground,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { styles } from "@/styles/_mainstyle";
+import { BACKGROUNDIMAGE, HEROLOGO } from "@/constants";
+import { useRouter } from "expo-router";
 
-export default function Index() {
+const Index = () => {
+  const router = useRouter();
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <ImageBackground
+          source={BACKGROUNDIMAGE}
+          style={styles.backgroundImageContainer}
+          resizeMode="cover"
+        >
+          <View style={styles.container}>
+            <View style={styles.logoContainer}>
+              <Image source={HEROLOGO} />
+            </View>
+
+            <View style={styles.titleContainer}>
+              <Text style={styles.titleText}>
+                Find your favorite place here
+              </Text>
+              <Text style={styles.titleSubText}>
+                The best prices for over 2{" "}
+              </Text>
+              <Text style={styles.titleSubText}>
+                million properties worldwide
+              </Text>
+            </View>
+
+            <View style={{ position: "absolute", bottom: 0, width: "100%" }}>
+              <View style={styles.buttonGroup}>
+                <TouchableOpacity
+                  style={styles.buttonPrimary}
+                  onPress={() => router.navigate("/join")}
+                >
+                  <Text style={styles.buttonPrimaryText}>Join here</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                  style={styles.buttonSecondary}
+                  onPress={() => router.navigate("/signin")}
+                >
+                  <Text style={styles.buttonSecondaryText}>Sign In</Text>
+                </TouchableOpacity>
+              </View>
+              <View style={{ alignItems: "center", paddingVertical: 20 }}>
+                <Text style={{ color: "white" }}>Continue to home</Text>
+              </View>
+            </View>
+          </View>
+        </ImageBackground>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
-}
+};
+
+export default Index;
